db097e2e77245761401c2cdb077c7727c79a04fd
Bhavna1607 <er.bhavna0710@gmail.com>
2025-09-06 00:05:10 +0530

    hero section spotlight moving border setup inside ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
@@ -100,7 +101,7 @@ export default function Home() {
     //   </footer>
     // </div>
 <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
- <h1 className="text-2xl text-center">Hello world</h1>
+ <HeroSection/>
 </main>
    
   );
