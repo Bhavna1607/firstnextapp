69ca68dff74f803debe0ddc0f9f5e230cb7465a9
Bhavna1607 <er.bhavna0710@gmail.com>
2025-09-09 18:42:20 +0530

    complted all ui implementation and next project

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      conatct us
+    </div>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpComingWebinar from "@/components/UpComingWebinar";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
@@ -100,9 +106,15 @@ export default function Home() {
     //     </a>
     //   </footer>
     // </div>
-<main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
- <HeroSection/>
-</main>
-   
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs/>
+      <TestimonialCards/>
+      <UpComingWebinar/>
+      <Instructors/>
+      <Footer/>
+    </main>
   );
 }
